8ea0d38b7dbc6cec0d8c45e13b2f7b3366825c91
Evan Filkins and Maly Phongsavanh <evan@>
2018-08-09 14:36:21 -0700

    Implement JS to show survey after gender selection.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#gender").submit(function(event){
+    event.preventDefault();
+    $(".survey2").show();
+  });
+  // New Submit Function for Result
+});
+
+  // var men = $("input#male[name=gender]:checked".val();
